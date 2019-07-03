aba4428bc3d468b2ef6ddf2feb87316fd45eb3d6
Marcio Paduan Donadio <marcio@slac.stanford.edu>
2019-07-03 11:03:22 -0700

    Added Mission Readiness iocs

---
+++ iocBoot/common/blenCommon.cmd
@@ -0,0 +1,150 @@
+# ===========================================
+#            ENVIRONMENT VARIABLES
+# ===========================================
+
+# ========================================================
+# Support Large Arrays/Waveforms; Number in Bytes
+# Please calculate the size of the largest waveform
+# that you support in your IOC.  Do not just copy numbers
+# from other apps.  This will only lead to an exhaustion
+# of resources and problems with your IOC.
+# The default maximum size for a channel access array is
+# 16K bytes.
+# ========================================================
+# Uncomment and set appropriate size for your application:
+
+# This IOC provides waveforms with the maximum size of 2800 DBR_TIME_DOUBLE
+# itens. For 64 bits systems it corresponds to 12 bytes per item.
+# 2800 * 12 = 33600.
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","34000")
+
+# YAML directory
+epicsEnvSet("YAML_DIR","${IOC_DATA}/${IOC}/firmware/yaml")
+#epicsEnvSet("YAML_DIR","${IOC_DATA}/${IOC}/firmware/marcio")
+# Yaml File
+epicsEnvSet("TOP_YAML","${YAML_DIR}/000TopLevel.yaml")
+epicsEnvSet("YAML_CONFIG_FILE", "${YAML_DIR}/config/defaults.yaml")
+#epicsEnvSet("YAML_CONFIG_FILE", "${IOC_DATA}/${IOC}/firmware/yaml/config/defaults.yaml")
+
+# BSA stream name must be identical to definition in yaml file
+epicsEnvSet("BSA_STREAM_YAML_NAME", "MrBlenBsaStream")
+
+# *********************************************
+# **** Environment variables for YCPSWASYN ****
+
+# Use Automatic generation of records from the YAML definition.
+# Usually it is 0.
+# 0 = No, 1 = Yes
+epicsEnvSet("AUTO_GEN", 0)
+
+# Automatically generated record prefix, case the previous option is 1
+epicsEnvSet("PREFIX","BLEN:${AREA}:${POS}:${INST}_")
+
+# Dictionary file for manually defined records
+epicsEnvSet("DICT_FILE", "yaml/blenMR.dict")
+
+# ===========================================
+#               DBD LOADING
+# ===========================================
+## Register all support components
+dbLoadDatabase("dbd/blen.dbd",0,0)
+blen_registerRecordDeviceDriver(pdbbase)
+
+# ===========================================
+#              DRIVER SETUP
+# ===========================================
+
+## Yaml Downloader
+DownloadYamlFile("${FPGA_IP}", "${YAML_DIR}")
+
+# Load yaml files do CPSW
+cpswLoadYamlFile("${TOP_YAML}", "NetIODev", "", "${FPGA_IP}")
+cpswLoadConfigFile("${YAML_CONFIG_FILE}", "mmio", "")
+
+# Driver setup for YCPSWAsyn
+# YCPSWASYNConfig(
+#    Port Name,                 # the name given to this port driver
+#    Root Path                  # OPTIONAL: Root path to start the generation. If empty, the Yaml root will be used
+#    Record name Prefix,        # Record name prefix
+#    Use DB Autogeneration,     # Set to 1 for autogeneration of records from the YAML definition. Set to 0 to disable it
+#    Load dictionary,           # Dictionary file path with registers to load. An empty string will disable this function
+YCPSWASYNConfig("cpsw", "", "${PREFIX}", "${AUTO_GEN}", "${DICT_FILE}")
+
+# Load drivers for TPR trigger, pattern, and crossbar control
+crossbarControlAsynDriverConfigure("crossbar", "mmio/AmcCarrierCore/AxiSy56040")
+tprTriggerAsynDriverConfigure("trig", "mmio/AmcCarrierCore")
+tprPatternAsynDriverConfigure("pattern", "mmio/AmcCarrierCore", "tstream")
+
+# FCOM network address and number of buffers
+fcomInit("${FCOM_NETWORK}", "20")
+
+# ===========================================
+#               DB LOADING
+# ===========================================
+
+# Manually created yCPSWasyn records
+dbLoadRecords("db/blenMR.db", "AREA=${AREA}, POS=${POS}:, INST=${INST}A, INST_NUM=A, PORT=cpsw, AMC=0")
+dbLoadRecords("db/blenMR.db", "AREA=${AREA}, POS=${POS}:, INST=${INST}B, INST_NUM=B, PORT=cpsw, AMC=1")
+
+# Switch on/off stream data from FPGA
+dbLoadRecords("db/streamControl.db", "AREA=${AREA}, POS=${POS}, INST=${INST}")
+
+# Records to manipulate waveforms from detectors
+dbLoadRecords("db/calculatedWF.db", "AREA=${AREA}, POS=${POS}, INST=${INST}A")
+dbLoadRecords("db/calculatedWF.db", "AREA=${AREA}, POS=${POS}, INST=${INST}B")
+dbLoadRecords("db/processRawWFHeader.db", "AREA=${AREA}, POS=${POS}, INST=${INST}A")
+dbLoadRecords("db/processRawWFHeader.db", "AREA=${AREA}, POS=${POS}, INST=${INST}B")
+dbLoadRecords("db/weightFunctionXAxis.db", "AREA=${AREA}, POS=${POS}, INST=${INST}A")
+dbLoadRecords("db/weightFunctionXAxis.db", "AREA=${AREA}, POS=${POS}, INST=${INST}B")
+
+# Filter control
+dbLoadRecords("db/blenFilterDecoders.db", "AREA=${AREA}, POS=${POS}")
+dbLoadRecords("db/blenFilters.db", "AREA=${AREA}, POS=${POS}, INST=${INST}")
+
+epicsEnvSet("PART_PV", "${AREA}:BL01")
+
+# Timing crossbar, pattern, and trigger
+dbLoadRecords("db/tprTrig.db",     "LOCA=${AREA}, IOC_UNIT=BL01, INST=2, PORT=trig")
+dbLoadRecords("db/tprPattern.db", "DEV=${IOC_NAME}, PORT=pattern")
+dbLoadRecords("db/crossbarCtrl.db", "DEV=EVR:${PART_PV}, PORT=crossbar")
+
+# BSA records
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=AIMAX")
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=BIMAX")
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=ARAW")
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=BRAW")
+
+#Save/Load configuration related records
+dbLoadRecords("db/saveLoadConfig.db", "P=BLEN:${AREA}:${POS}:, PORT=cpsw")
+
+# Automatic initialization
+#dbLoadRecords("db/monitorFPGAReboot.db", "P=BLEN:${AREA}:${POS}, KEY=840202")
+
+# **********************************************************************
+# **** Load iocAdmin databases to support IOC Health and monitoring ****
+dbLoadRecords("db/iocAdminSoft.db","IOC=${IOC_NAME}")
+dbLoadRecords("db/iocAdminScanMon.db","IOC=${IOC_NAME}")
+
+# The following database is a result of a python parser
+# which looks at RELEASE_SITE and RELEASE to discover
+# versions of software your IOC is referencing
+# The python parser is part of iocAdmin
+dbLoadRecords("db/iocRelease.db","IOC=${IOC_NAME}")
+
+# ===========================================
+#          CHANNEL ACESS SECURITY
+# ===========================================
+# This is required if you use caPutLog.
+# Set access security filea
+# Load common LCLS Access Configuration File
+< ${ACF_INIT}
+
+# ===========================================
+#           SETUP AUTOSAVE/RESTORE
+# ===========================================
+< iocBoot/common/init_restore_soft.cmd
+
+# ===========================================
+#           ARCHIVER FILES
+# ===========================================
+system("cp ${TOP}/archive/${IOC}.archive ${IOC_DATA}/${IOC}/archive")
---
+++ iocBoot/common/init_restore_soft.cmd
@@ -0,0 +1,58 @@
+#==============================================================
+#
+#  Abs:  Autosave initalization (for a soft IOC)
+#
+#  Name: init_restore_soft.cmd    
+#
+#  Facility:  LCLS CAMAC Controls
+#
+#  Auth: 27-Jan-2011, Kristi Luchini  (LUCHINI)
+#  Rev:  dd-mmm-yyyy, Reviewer's Name (USERNAME)
+#--------------------------------------------------------------
+#  Mod:
+#        dd-mmm-yyyy, Reviewer's Name (USERNAME)
+#          comment
+#
+#==============================================================
+#
+save_restoreSet_Debug(0)
+
+# Specify where request and save_restore files can be found
+set_requestfile_path("${IOC_DATA}/${IOC}/autosave-req")
+set_requestfile_path("${TOP}/restore", "")
+set_requestfile_path("${TOP}/iocBoot/${IOC}", "")
+set_savefile_path("${IOC_DATA}/${IOC}/autosave")
+
+# Status-PV prefix, so save_restore can find its status PVs.
+save_restoreSet_status_prefix("${IOC_NAME}:")
+
+# Load bumpless reboot status database
+dbLoadRecords("db/save_restoreStatus.db","P=${IOC_NAME}:")
+
+# Ok to restore a save set that had missing values (no CA connection to PV)?
+# Ok to save a file if some CA connections are bad?
+save_restoreSet_IncompleteSetsOk(1)
+
+# In the restore operation, a copy of the save file will be written.  The
+# file name can look like "auto_settings.sav.bu", and be overwritten every
+# reboot, or it can look like "auto_settings.sav_020306-083522" (this is what
+# is meant by a dated backup file) and every reboot will write a new copy.
+#save_restoreSet_DatedBackupFiles(1)
+
+# Specify what save files should be restored and when.
+# Note: up to eight files can be specified for each pass.
+set_pass0_restoreFile("info_positions.sav")
+set_pass0_restoreFile("info_settings.sav")
+set_pass1_restoreFile("info_settings.sav")
+set_pass1_restoreFile("blen_manual.sav")
+
+# Number of sequenced backup files (e.g., 'info_settings.sav0') to write
+save_restoreSet_NumSeqFiles(3)
+
+# Time interval between sequenced backups
+save_restoreSet_SeqPeriodInSeconds(600)
+
+# Time between failed .sav-file write and the retry.
+save_restoreSet_RetrySeconds(60)
+
+# End of script
