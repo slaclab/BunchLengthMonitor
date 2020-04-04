5bfc78899eab2a639e1132248fbaa9713b9a2086
Janos Vamosi <jvamosi@slac.stanford.edu>
2020-04-04 08:57:59 -0700

    Added FACET-II devices

---
+++ iocBoot/sioc-li11-bl01/st.cmd
@@ -0,0 +1,76 @@
+#!../../bin/linuxRT-x86_64/blen
+
+## You may have to change blen to something else
+## everywhere it appears in this file
+
+< envPaths
+
+# ===========================================
+#            ENVIRONMENT VARIABLES
+# ===========================================
+
+# Area, position, and instrument names to be used in record names
+epicsEnvSet("AREA", "LI11")
+epicsEnvSet("POS", "356")
+epicsEnvSet("INST", "BL11356")
+
+# Address of the FCOM network
+epicsEnvSet("FCOM_NETWORK", "239.219.8.0")
+
+# TMIT PV to read the value from, by using FCOM
+epicsEnvSet("TMIT_PV", "BPMS:LI11:358:TMIT")
+
+# FPGA IP address for CPSW
+epicsEnvSet("FPGA_IP", "10.0.1.104")
+# Port number to send TMIT data to the FPGA
+epicsEnvSet("IP_PORT_TMIT", "8195")
+
+# IOC name for IOC admin
+epicsEnvSet(IOC_NAME,"SIOC:$(AREA):BL01")
+
+cd ${TOP}
+
+< iocBoot/common/blenCommon.cmd
+
+# ===========================================
+#               ASYN MASKS
+# ===========================================
+
+# ***********************************
+# * Asyn Masks for all Asyn drivers *
+
+#asynSetTraceMask(cpsw, -1, 9)
+#asynSetTraceMask(crossbar, -1, 9)
+#asynSetTraceMask(trig, -1, 9)
+#asynSetTraceMask(pattern, -1, 9)
+
+# ===========================================
+#               DB LOADING
+# ===========================================
+
+
+# ===========================================
+#           SETUP AUTOSAVE/RESTORE
+# ===========================================
+
+# ===========================================
+#               IOC INIT
+# ===========================================
+iocInit()
+
+# Enforce RTM timing
+crossbarControl "FPGA" "LCLS1"
+
+# Turn on caPutLogging:
+# Log values only on change to the iocLogServer:
+caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}")
+caPutLogShow(2)
+
+# blenConfigure parameters:
+# 1 - Station name
+# 2 - BSA stream name must be identical to definition in yaml file
+# 3 - PV used to get TMIT from FCOM
+# 4 - IP address and port to send TMIT information to ATCA
+blenConfigure "BLEN:${AREA}:${POS}" "${BSA_STREAM_YAML_NAME}" "${TMIT_PV}" "${FPGA_IP}:${IP_PORT_TMIT}"
+
+< iocBoot/common/start_restore_soft.cmd
