a80614cd5b07ba2561c0071c267663fc76f99775
rreno <rreno@slac.stanford.edu>
2020-04-24 10:13:45 -0700

    Added loading blen.db in blenCommonLCLS2.cmd

--- iocBoot/common/blenCommonLCLS2.cmd
+++ iocBoot/common/blenCommonLCLS2.cmd
@@ -3,8 +3,11 @@
 # ===========================================
 
 # Manually created yCPSWasyn records
-dbLoadRecords("db/blenLCLS2.db", "P=BLEN:${AREA}:${POS}:${INST}A, PORT=cpsw, AMC=0")
-dbLoadRecords("db/blenLCLS2.db", "P=BLEN:${AREA}:${POS}:${INST}B, PORT=cpsw, AMC=1")
+dbLoadRecords("db/blen.db", "P=BLEN:${AREA}:${POS}:${INST}A:, PORT=cpsw, AMC=0")
+dbLoadRecords("db/blen.db", "P=BLEN:${AREA}:${POS}:${INST}B:, PORT=cpsw, AMC=1")
+
+dbLoadRecords("db/blenLCLS2.db", "P=BLEN:${AREA}:${POS}:${INST}A:, PORT=cpsw, AMC=0")
+dbLoadRecords("db/blenLCLS2.db", "P=BLEN:${AREA}:${POS}:${INST}B:, PORT=cpsw, AMC=1")
 
 # Filter control
 dbLoadRecords("db/statusBit.db", "P=$(AREA):$(POS), P0=$(AREA)$(POS):AMC0, P1=$(AREA):$(POS):AMC1")
