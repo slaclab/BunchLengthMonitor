6e033b4db7b30fd9961880cee3f0ffed380a763b
rreno <rreno@slac.stanford.edu>
2020-05-01 22:20:34 -0700

    asubWeightXAxis -> changed clkFreq from pointer to long int
    This is a style change. clkFreq was never used as a pointer so it is now
    declared as a `long` and gets its value from a dereference instead of being
    dereferenced in the conversion calculation from ADC clock ticks to ns.

--- blenApp/src/asubWeightXAxis.c
+++ blenApp/src/asubWeightXAxis.c
@@ -3,16 +3,16 @@
 #include <aSubRecord.h>
 
 static long calcTimeArray(aSubRecord *pasub) {
-    long i, *clkFreq;
+    long i, clkFreq;
     double sum = 0, timeNs;
 
     pasub->pact = 1;
 
-    clkFreq = (long *)pasub->a;
+    clkFreq = *((long *)pasub->a);
 
     // Calculate the time in ns between each sample, giving the clock
     // frequency in Hertz
-    timeNs = 1000000000 / (2.0 * *clkFreq);
+    timeNs = 1000000000 / (2.0 * clkFreq);
 
     // Build array with the relative time for each sample since the first one
     for (i=0; i < pasub->nova; ++i) {
