86f1869a0c2fa860878d077a7e40a7cffe4573be
rreno <rreno@slac.stanford.edu>
2020-05-06 13:04:13 -0700

    Checked in iocBoot/common scripts from R4.0.0b0

---
+++ iocBoot/common/blenCommonLCLS2.cmd
@@ -0,0 +1,19 @@
+# ===========================================
+#               DB LOADING
+# ===========================================
+
+# Manually created yCPSWasyn records
+dbLoadRecords("db/blen.db", "P=BLEN:${AREA}:${POS}:${INST}A, PORT=cpsw, AMC=0")
+dbLoadRecords("db/blen.db", "P=BLEN:${AREA}:${POS}:${INST}B, PORT=cpsw, AMC=1")
+
+dbLoadRecords("db/blenLCLS2.db", "P=BLEN:${AREA}:${POS}, PORT=cpsw")
+dbLoadRecords("db/blenLCLS2.db", "P=BLEN:${AREA}:${POS}, PORT=cpsw")
+
+# Filter control
+dbLoadRecords("db/statusBit.db", "P=BLEN:$(AREA):$(POS), P0=BLEN:$(AREA):$(POS):$(INST)A, P1=BLEN:$(AREA):$(POS):$(INST)B")
+
+# BSA records
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=AIMAX, SINK_SIZE=1, LNK=''")
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=BIMAX, SINK_SIZE=1, LNK=''")
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=ARAW, SINK_SIZE=1, LNK=''")
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=BRAW, SINK_SIZE=1, LNK=''")
---
+++ iocBoot/common/blenCommonMR.cmd
@@ -0,0 +1,39 @@
+# ===========================================
+#            ENVIRONMENT VARIABLES
+# ===========================================
+
+# BSA stream name must be identical to definition in yaml file
+epicsEnvSet("BSA_STREAM_YAML_NAME", "MrBlenBsaStream")
+
+# ===========================================
+#              DRIVER SETUP
+# ===========================================
+
+# Load drivers for TPR pattern
+tprPatternAsynDriverConfigure("pattern", "mmio/AmcCarrierCore", "tstream")
+
+# FCOM network address and number of buffers
+fcomInit("${FCOM_NETWORK}", "20")
+
+# ===========================================
+#               DB LOADING
+# ===========================================
+
+# Manually created yCPSWasyn records
+dbLoadRecords("db/blenMR.db", "AREA=${AREA}, POS=${POS}, INST=${INST}A, INST_NUM=A, PORT=cpsw, AMC=0")
+dbLoadRecords("db/blenMR.db", "AREA=${AREA}, POS=${POS}, INST=${INST}B, INST_NUM=B, PORT=cpsw, AMC=1")
+
+# Filter control
+dbLoadRecords("db/blenFilterDecoders.db", "AREA=${AREA}, POS=${POS}")
+dbLoadRecords("db/blenFilters.db", "AREA=${AREA}, POS=${POS}, INST=${INST}")
+
+# Switch on/off stream data from FPGA
+dbLoadRecords("db/streamControl.db", "AREA=${AREA}, POS=${POS}, INST=${INST}")
+
+dbLoadRecords("db/tprPattern.db",  "LOCA=${AREA}, IOC_UNIT=${IOC_UNIT}, INST=2, PORT=pattern")
+
+# BSA records
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=AIMAX, SINK_SIZE=1")
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=BIMAX, SINK_SIZE=1")
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=ARAW, SINK_SIZE=1")
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=BRAW, SINK_SIZE=1")
