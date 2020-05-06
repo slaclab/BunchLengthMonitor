3e3a3e1fdcd99fa179d4756999ef45c9bfb6b124
rreno <rreno@slac.stanford.edu>
2020-05-06 13:03:33 -0700

    Further work on database reorg. Checked in iocBoot scripts from R4.0.0b0 branch

--- iocBoot/common/blenCommon.cmd
+++ iocBoot/common/blenCommon.cmd
@@ -2,32 +2,10 @@
 #            ENVIRONMENT VARIABLES
 # ===========================================
 
-# ========================================================
-# Support Large Arrays/Waveforms; Number in Bytes
-# Please calculate the size of the largest waveform
-# that you support in your IOC.  Do not just copy numbers
-# from other apps.  This will only lead to an exhaustion
-# of resources and problems with your IOC.
-# The default maximum size for a channel access array is
-# 16K bytes.
-# ========================================================
-# Uncomment and set appropriate size for your application:
 
-# This IOC provides waveforms with the maximum size of 2800 DBR_TIME_DOUBLE
-# itens. For 64 bits systems it corresponds to 12 bytes per item.
-# 2800 * 12 = 33600.
-epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","34000")
+# For crossbarControl DB prefix
+epicsEnvSet("PART_PV", "${AREA}:${IOC_UNIT}")
 
-# YAML directory
-epicsEnvSet("YAML_DIR","${IOC_DATA}/${IOC}/firmware/yaml")
-#epicsEnvSet("YAML_DIR","${IOC_DATA}/${IOC}/firmware/marcio")
-# Yaml File
-epicsEnvSet("TOP_YAML","${YAML_DIR}/000TopLevel.yaml")
-epicsEnvSet("YAML_CONFIG_FILE", "${YAML_DIR}/config/defaults.yaml")
-#epicsEnvSet("YAML_CONFIG_FILE", "${IOC_DATA}/${IOC}/firmware/yaml/config/defaults.yaml")
-
-# BSA stream name must be identical to definition in yaml file
-epicsEnvSet("BSA_STREAM_YAML_NAME", "MrBlenBsaStream")
 
 # *********************************************
 # **** Environment variables for YCPSWASYN ****
@@ -39,9 +17,6 @@ epicsEnvSet("AUTO_GEN", 0)
 
 # Automatically generated record prefix, case the previous option is 1
 epicsEnvSet("PREFIX","BLEN:${AREA}:${POS}:${INST}_")
-
-# Dictionary file for manually defined records
-epicsEnvSet("DICT_FILE", "yaml/blenMR.dict")
 
 # ===========================================
 #               DBD LOADING
@@ -70,24 +45,18 @@ cpswLoadConfigFile("${YAML_CONFIG_FILE}", "mmio", "")
 #    Load dictionary,           # Dictionary file path with registers to load. An empty string will disable this function
 YCPSWASYNConfig("cpsw", "", "${PREFIX}", "${AUTO_GEN}", "${DICT_FILE}")
 
-# Load drivers for TPR trigger, pattern, and crossbar control
+# Load drivers for TPR trigger and crossbar control
 crossbarControlAsynDriverConfigure("crossbar", "mmio/AmcCarrierCore/AxiSy56040")
 tprTriggerAsynDriverConfigure("trig", "mmio/AmcCarrierCore")
-tprPatternAsynDriverConfigure("pattern", "mmio/AmcCarrierCore", "tstream")
 
-# FCOM network address and number of buffers
-fcomInit("${FCOM_NETWORK}", "20")
 
 # ===========================================
 #               DB LOADING
 # ===========================================
 
-# Manually created yCPSWasyn records
-dbLoadRecords("db/blenMR.db", "AREA=${AREA}, POS=${POS}, INST=${INST}A, INST_NUM=A, PORT=cpsw, AMC=0")
-dbLoadRecords("db/blenMR.db", "AREA=${AREA}, POS=${POS}, INST=${INST}B, INST_NUM=B, PORT=cpsw, AMC=1")
-
-# Switch on/off stream data from FPGA
-dbLoadRecords("db/streamControl.db", "AREA=${AREA}, POS=${POS}, INST=${INST}")
+# main blen database
+dbLoadRecords("db/blen.db", "P=BLEN:$(AREA):$(POS), PORT=cpsw, AMC=0")
+dbLoadRecords("db/blen.db", "P=BLEN:$(AREA):$(POS), PORT=cpsw, AMC=1")
 
 # Records to manipulate waveforms from detectors
 dbLoadRecords("db/calculatedWF.db", "AREA=${AREA}, POS=${POS}, INST=${INST}A")
@@ -97,28 +66,16 @@ dbLoadRecords("db/processRawWFHeader.db", "AREA=${AREA}, POS=${POS}, INST=${INST
 dbLoadRecords("db/weightFunctionXAxis.db", "AREA=${AREA}, POS=${POS}, INST=${INST}A")
 dbLoadRecords("db/weightFunctionXAxis.db", "AREA=${AREA}, POS=${POS}, INST=${INST}B")
 
-# Filter control
-dbLoadRecords("db/blenFilterDecoders.db", "AREA=${AREA}, POS=${POS}")
-dbLoadRecords("db/blenFilters.db", "AREA=${AREA}, POS=${POS}, INST=${INST}")
 
-epicsEnvSet("PART_PV", "${AREA}:BL01")
-
-# Timing crossbar, pattern, and trigger
-dbLoadRecords("db/tprTrig.db",     "LOCA=${AREA}, IOC_UNIT=BL01, INST=2, PORT=trig")
-dbLoadRecords("db/tprPattern.db",  "LOCA=${AREA}, IOC_UNIT=BL01, INST=2, PORT=pattern")
+# Timing crossbar and trigger
+dbLoadRecords("db/tprTrig.db",     "LOCA=${AREA}, IOC_UNIT=${IOC_UNIT}, INST=2, PORT=trig")
 dbLoadRecords("db/crossbarCtrl.db", "DEV=EVR:${PART_PV}, PORT=crossbar")
 
-# BSA records
-dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=AIMAX, SINK_SIZE=1")
-dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=BIMAX, SINK_SIZE=1")
-dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=ARAW, SINK_SIZE=1")
-dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=BRAW, SINK_SIZE=1")
 
-#Save/Load configuration related records
+#Save/Load configuration and Monitor FPGA reboots
 dbLoadRecords("db/saveLoadConfig.db", "P=BLEN:${AREA}:${POS}:, PORT=cpsw, SAVE_FILE=/tmp/configDump.yaml, LOAD_FILE=${IOC_DATA}/${IOC}/firmware/yaml/config/defaults.yaml")
 
-# Automatic initialization
-#dbLoadRecords("db/monitorFPGAReboot.db", "P=BLEN:${AREA}:${POS}, KEY=840202")
+dbLoadRecords("db/monitorFPGAReboot.db", "P=BLEN:${AREA}:${POS}, KEY=0xFC067333")
 
 # **********************************************************************
 # **** Load iocAdmin databases to support IOC Health and monitoring ****
@@ -148,3 +105,8 @@ dbLoadRecords("db/iocRelease.db","IOC=${IOC_NAME}")
 #           ARCHIVER FILES
 # ===========================================
 system("cp ${TOP}/archive/${IOC}.archive ${IOC_DATA}/${IOC}/archive")
+
+# ===========================================
+#          LOAD MR or LCLS2 CONFIG 
+# ===========================================
+< iocBoot/common/blenCommon$(BLEN_VERSION).cmd
---
+++ iocBoot/sioc-b084-bl03/st.cmd
@@ -0,0 +1,71 @@
+#!../../bin/linuxRT-x86_64/blen
+
+## You may have to change blen to something else
+## everywhere it appears in this file
+
+< envPaths
+
+# ===========================================
+#            ENVIRONMENT VARIABLES
+# ===========================================
+
+# Area, position, and instrument names to be used in record names
+epicsEnvSet("AREA", "B084")
+epicsEnvSet("POS", "886")
+epicsEnvSet("INST", "BL21")
+epicsEnvSet("IOC_UNIT", "BL03")
+
+# YAML directory
+epicsEnvSet("YAML_DIR","${IOC_DATA}/${IOC}/firmware/yaml")
+
+# Yaml File
+epicsEnvSet("TOP_YAML","${YAML_DIR}/000TopLevel.yaml")
+epicsEnvSet("YAML_CONFIG_FILE", "${YAML_DIR}/config/defaultsPyro.yaml")
+
+# FPGA IP address for CPSW
+epicsEnvSet("FPGA_IP", "10.0.1.107")
+
+# IOC name for IOC admin
+epicsEnvSet(IOC_NAME,"SIOC:$(AREA):${IOC_UNIT}")
+
+# Which version of the Application to use - "MR" or "LCLS2"
+epicsEnvSet("BLEN_VERSION", "LCLS2")
+epicsEnvSet("DICT_FILE", "yaml/blenLCLS2.dict")
+
+cd ${TOP}
+
+< iocBoot/common/blenCommon.cmd
+
+# ===========================================
+#               ASYN MASKS
+# ===========================================
+
+# ***********************************
+# * Asyn Masks for all Asyn drivers *
+
+#asynSetTraceMask(cpsw, -1, 9)
+#asynSetTraceMask(crossbar, -1, 9)
+#asynSetTraceMask(trig, -1, 9)
+#asynSetTraceMask(pattern, -1, 9)
+
+# ===========================================
+#               DB LOADING
+# ===========================================
+
+
+# ===========================================
+#           SETUP AUTOSAVE/RESTORE
+# ===========================================
+
+# ===========================================
+#               IOC INIT
+# ===========================================
+iocInit()
+
+# Enforce RTM timing
+crossbarControl "FPGA" "${BLEN_VERSION}"
+
+# Turn on caPutLogging:
+# Log values only on change to the iocLogServer:
+caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}")
+caPutLogShow(2)
