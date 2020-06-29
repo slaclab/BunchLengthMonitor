06d213459d7c3a09cb02647dfa411acf649f31b8
rreno <rreno@slac.stanford.edu>
2020-06-29 13:38:28 -0700

    Added startup script for BC2B SIOC

---
+++ iocBoot/sioc-bc2b-bl01/st.cmd
@@ -0,0 +1,66 @@
+#!../../bin/linuxRT-x86_64/blen
+
+## You may have to change blen to something else
+## everywhere it appears in this file
+
+< envPaths
+
+# ===========================================
+#            ENVIRONMENT VARIABLES
+# ===========================================
+
+# Area, position, and instrument names to be used in record names
+epicsEnvSet("AREA", "BC2B")
+epicsEnvSet("POS", "950")
+epicsEnvSet("INST", "BZ21B")
+epicsEnvSet("IOC_UNIT", "BL01")
+
+# YAML directory
+epicsEnvSet("YAML_DIR","$(IOC_DATA)/$(IOC)/firmware/yaml")
+
+# Yaml File
+epicsEnvSet("TOP_YAML","$(YAML_DIR)/000TopLevel.yaml")
+epicsEnvSet("YAML_CONFIG_FILE", "$(YAML_DIR)/config/defaultsPyro.yaml")
+
+# FPGA IP address for CPSW
+epicsEnvSet("FPGA_IP", "10.0.1.103")
+
+# IOC name for IOC admin
+epicsEnvSet(IOC_NAME,"SIOC:$(AREA):$(IOC_UNIT)")
+
+# Which version of the Application to use - "LCLS1" or "LCLS2"
+epicsEnvSet("BLEN_VERSION", "LCLS2")
+epicsEnvSet("DICT_FILE", "yaml/blenLCLS2.dict")
+
+cd $(TOP)
+
+< iocBoot/common/blenCommon.cmd
+
+# ===========================================
+#               ASYN MASKS
+# ===========================================
+
+
+# ===========================================
+#               DB LOADING
+# ===========================================
+
+
+# ===========================================
+#           SETUP AUTOSAVE/RESTORE
+# ===========================================
+
+# ===========================================
+#               IOC INIT
+# ===========================================
+iocInit()
+
+# Enforce RTM timing
+crossbarControl "FPGA" "$(BLEN_VERSION)"
+
+# Turn on caPutLogging:
+# Log values only on change to the iocLogServer:
+caPutLogInit("$(EPICS_CA_PUT_LOG_ADDR)")
+caPutLogShow(2)
+
+< iocBoot/common/start_restore_soft.cmd
