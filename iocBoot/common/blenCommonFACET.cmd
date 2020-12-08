1d88dfdc4ec8b9cb17a522ad779f6a870a0b04a1
rreno <rreno@slac.stanford.edu>
2020-12-08 13:06:11 -0800

    Started making FACET specific startup scripts

---
+++ iocBoot/common/blenCommonFACET.cmd
@@ -0,0 +1,51 @@
+# ===========================================
+#            ENVIRONMENT VARIABLES
+# ===========================================
+
+# BSA stream name must be identical to definition in yaml file
+epicsEnvSet("BSA_STREAM_YAML_NAME", "MrBlenBsaStream")
+
+# ===========================================
+#              DRIVER SETUP
+# ===========================================
+
+# Load drivers for TPR pattern and crossbarControl
+crossbarControlAsynDriverConfigure("crossbar", "mmio/AmcCarrierCore/AxiSy56040")
+tprPatternAsynDriverConfigure("pattern", "mmio/AmcCarrierCore", "tstream")
+
+# FCOM network address and number of buffers
+fcomInit("${FCOM_NETWORK}", "20")
+
+# ===========================================
+#               DB LOADING
+# ===========================================
+
+# Manually created yCPSWasyn records
+dbLoadRecords("db/blenMR.db", "AREA=${AREA}, POS=${POS}, INST=${INST}A, INST_NUM=A, PORT=cpsw, AMC=0")
+dbLoadRecords("db/blenMR.db", "AREA=${AREA}, POS=${POS}, INST=${INST}B, INST_NUM=B, PORT=cpsw, AMC=1")
+
+dbLoadRecords("db/tprPattern.db",  "LOCA=${AREA}, IOC_UNIT=${IOC_UNIT}, INST=2, PORT=pattern")
+
+# BSA records
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=AIMAX, SINK_SIZE=1")
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=BIMAX, SINK_SIZE=1")
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=ARAW, SINK_SIZE=1")
+dbLoadRecords("db/Bsa.db", "DEVICE=BLEN:${AREA}:${POS},ATRB=BRAW, SINK_SIZE=1")
+
+# tpr high level PVs
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=00,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG00:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=01,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG01:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=02,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG02:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=03,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG03:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=04,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG04:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=05,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG05:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=06,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG06:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=07,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG07:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=08,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG08:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=09,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG09:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=10,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG10:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=11,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG11:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=12,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG12:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=13,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG13:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=14,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG14:,PORT=trig")
+dbLoadRecords("db/tprDeviceNamePV.db","LOCA=$(AREA),IOC_UNIT=$(IOC_UNIT),INST=$(ATCA_SLOT),SYS=SYS0,NN=15,DEV_PREFIX=BLEN:$(AREA):$(POS):TRG15:,PORT=trig")
--- iocBoot/sioc-in10-bl01/st.cmd
+++ iocBoot/sioc-in10-bl01/st.cmd
@@ -13,6 +13,17 @@
 epicsEnvSet("AREA", "IN10")
 epicsEnvSet("POS", "596")
 epicsEnvSet("INST", "BZ10596")
+epicsEnvSet("IOC_UNIT", "BL01")
+epicsEnvSet("ATCA_SLOT", "5")
+
+# YAML directory
+epicsEnvSet("YAML_DIR", "$(IOC_DATA)/$(IOC)/firmware/yaml")
+
+# YAML file
+epicsEnvSet("TOP_YAML", "$(YAML_DIR)/000TopLevel.yaml")
+epicsEnvSet("YAML_CONFIG_FILE", "$(YAML_DIR)/config/defaultsPyro.yaml")
+
+epicsEnvSet("FPGA_IP", "10.0.1.105")
 
 # Address of the FCOM network
 epicsEnvSet("FCOM_NETWORK", "239.219.8.0")
@@ -20,38 +31,19 @@ epicsEnvSet("FCOM_NETWORK", "239.219.8.0")
 # TMIT PV to read the value from, by using FCOM
 epicsEnvSet("TMIT_PV", "BPMS:IN10:581:TMIT")
 
-# FPGA IP address for CPSW
-epicsEnvSet("FPGA_IP", "10.0.1.105")
 # Port number to send TMIT data to the FPGA
 epicsEnvSet("IP_PORT_TMIT", "8195")
 
 # IOC name for IOC admin
 epicsEnvSet(IOC_NAME,"SIOC:$(AREA):BL01")
 
-cd ${TOP}
+# Which version of the Application to use - "FACET", "LCLS1", or "LCLS2"
+epicsEnvSet("BLEN_VERSION", "FACET")
+epicsEnvSet("DICT_FILE", "yaml/blenMR.dict")
+
+cd $(TOP)
 
 < iocBoot/common/blenCommon.cmd
-
-# ===========================================
-#               ASYN MASKS
-# ===========================================
-
-# ***********************************
-# * Asyn Masks for all Asyn drivers *
-
-#asynSetTraceMask(cpsw, -1, 9)
-#asynSetTraceMask(crossbar, -1, 9)
-#asynSetTraceMask(trig, -1, 9)
-#asynSetTraceMask(pattern, -1, 9)
-
-# ===========================================
-#               DB LOADING
-# ===========================================
-
-
-# ===========================================
-#           SETUP AUTOSAVE/RESTORE
-# ===========================================
 
 # ===========================================
 #               IOC INIT
@@ -59,18 +51,19 @@ cd ${TOP}
 iocInit()
 
 # Enforce RTM timing
-crossbarControl "FPGA" "LCLS1"
+# We're not in slot 2 so use backplane timing
+crossbarControl("FPGA" "BP")
 
 # Turn on caPutLogging:
 # Log values only on change to the iocLogServer:
 caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}")
 caPutLogShow(2)
 
-# blenConfigure parameters:
+# blenConfigureMR parameters:
 # 1 - Station name
-# 2 - BSA stream name must be identical to definition in yaml file
+# 2 - BSA stream name (must be identical to definition in yaml file)
 # 3 - PV used to get TMIT from FCOM
 # 4 - IP address and port to send TMIT information to ATCA
-blenConfigure "BLEN:${AREA}:${POS}" "${BSA_STREAM_YAML_NAME}" "${TMIT_PV}" "${FPGA_IP}:${IP_PORT_TMIT}"
+blenConfigureMR("BLEN:$(AREA):$(POS)" "$(BSA_STREAM_YAML_NAME)" "$(TMIT_PV)" "$(FPGA_IP):$(IP_PORT_TMIT)")
 
 < iocBoot/common/start_restore_soft.cmd
