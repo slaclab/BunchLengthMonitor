aedbb774358a02aae3c93ff1b9379bcc10662187
rreno <rreno@slac.stanford.edu>
2021-01-11 09:25:34 -0800

    Addded AppType + Location to FPGA Config display.

--- blenApp/ui/fpga_config.py
+++ blenApp/ui/fpga_config.py
@@ -1,9 +1,11 @@
-import os, subprocess
+import os
+import subprocess
+
+from enum import Enum
 
 from epics import PV
 
 from pydm import Display
-
 
 class FPGAConfig(Display):
     def __init__(self, parent=None, args=None, macros=None):
@@ -12,6 +14,7 @@ class FPGAConfig(Display):
         self.connect_pvs()
 
         self.ui.tree_gui_btn.clicked.connect(self.handle_tree_gui)
+        self.setup_location_label()
 
     def connect_pvs(self):
         self.sioc_pv_prefix = 'SIOC:{}:{}'.format(\
@@ -25,6 +28,29 @@ class FPGAConfig(Display):
         self.cpu = PV('{}:CPU'.format(self.sioc_pv_prefix))
         self.shm = PV('{}:SHM'.format(self.sioc_pv_prefix))
         self.atca_slot = PV('{}:ATCA_SLOT'.format(self.sioc_pv_prefix))
+
+    def setup_location_label(self):
+        """
+        Convert AppType and Location labels from integers to strings.
+        The values are defined in the YAML config.
+        """
+        pyro = 0
+        gap = 1
+
+        app_type = PV("{}:AppTypeRBV".format(self.blen_pv_prefix)).get()
+        loc = PV("{}:LocationRBV".format(self.blen_pv_prefix)).get()
+        app_type_txt = ""
+        loc_txt = ""
+
+        if app_type == pyro:
+            app_type_txt = "Pyrodetector"
+            loc_txt = "BC1" if loc == 0 else "BC2"
+        else:
+            app_type_txt = "Gap Diode"
+            loc_txt = "Injector" if loc == 0 else "BC1"
+
+        self.ui.appType.setText(app_type_txt)
+        self.ui.location.setText(loc_txt)
 
     def handle_tree_gui(self):
         try:
