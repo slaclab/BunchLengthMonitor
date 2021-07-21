9592b119aa2370d139ffb1ea084aff2b42e8125d
TaineEriksen <taine@slac.stanford.edu>
2021-07-21 12:40:42 -0700

    New LCLS2 style boot file for FACET gap diode

--- iocBoot/sioc-in10-bl01/st.cmd
+++ iocBoot/sioc-in10-bl01/st.cmd
@@ -18,33 +18,51 @@ epicsEnvSet("ATCA_SLOT", "5")
 epicsEnvSet("BLEN_ASYN_PORT", "ATCA$(ATCA_SLOT)")
 
 # YAML directory
-epicsEnvSet("YAML_DIR", "$(IOC_DATA)/$(IOC)/firmware/yaml")
+epicsEnvSet("YAML_DIR","$(IOC_DATA)/$(IOC)/yaml")
 
-# YAML file
-epicsEnvSet("TOP_YAML", "$(YAML_DIR)/000TopLevel.yaml")
-epicsEnvSet("YAML_CONFIG_FILE", "$(YAML_DIR)/config/defaults.yaml")
+# Yaml File
+epicsEnvSet("TOP_YAML","$(YAML_DIR)/000TopLevel.yaml")
+epicsEnvSet("YAML_CONFIG_FILE", "$(YAML_DIR)/config/defaultsGap.yaml")
 
 epicsEnvSet("FPGA_IP", "10.0.1.105")
 
-# Address of the FCOM network
-epicsEnvSet("FCOM_NETWORK", "224.0.0.0")
-
-# TMIT PV to read the value from, by using FCOM
-epicsEnvSet("TMIT_PV", "BPMS:IN10:581:TMIT")
-
-# Port number to send TMIT data to the FPGA
-epicsEnvSet("IP_PORT_TMIT", "8195")
-
 # IOC name for IOC admin
-epicsEnvSet(IOC_NAME,"SIOC:$(AREA):BL01")
+epicsEnvSet(IOC_NAME,"SIOC:$(AREA):$(IOC_UNIT)")
 
-# Which version of the Application to use - "FACET", "LCLS1", or "LCLS2"
-epicsEnvSet("BLEN_VERSION", "FACET")
-epicsEnvSet("DICT_FILE", "yaml/blenMR.dict")
+# Which version of the Application to use - "LCLS1" or "LCLS2"
+epicsEnvSet("BLEN_VERSION", "LCLS2")
+epicsEnvSet("DICT_FILE", "yaml/blenGapDiode.dict")
 
 cd $(TOP)
 
 < iocBoot/common/blenCommon.cmd
+
+# Additional Waveform containers for ch1 raw data on AMC0, AMC1
+dbLoadRecords("db/gap_streams.db", "P=BLEN:$(AREA):$(POS):$(INST)C, PORT=${BLEN_ASYN_PORT}, AMC=0")
+dbLoadRecords("db/gap_streams.db", "P=BLEN:$(AREA):$(POS):$(INST)D, PORT=${BLEN_ASYN_PORT}, AMC=1")
+
+# Additional offset coefficients for AMC0, AMC1 ch1 raw data
+dbLoadRecords("db/blen_gap_coefI2.db", "P=BLEN:$(AREA):$(POS):$(INST)A, PORT=${BLEN_ASYN_PORT}, AMC=0")
+dbLoadRecords("db/blen_gap_coefI2.db", "P=BLEN:$(AREA):$(POS):$(INST)B, PORT=${BLEN_ASYN_PORT}, AMC=1")
+
+# Additional per-ADC variables for ch1 on AMC0, AMC1
+dbLoadRecords("db/blen_gap_DspPreprocConfig.db", "P=BLEN:$(AREA):$(POS):$(INST)C, PORT=${BLEN_ASYN_PORT}, AMC=0")
+dbLoadRecords("db/blen_gap_DspPreprocConfig.db", "P=BLEN:$(AREA):$(POS):$(INST)D, PORT=${BLEN_ASYN_PORT}, AMC=1")
+
+# WeightFunction calculations supporting additio of ch1 on AMC0, AMC1
+dbLoadRecords("db/calculatedWF.db", "AREA=$(AREA), POS=$(POS), INST=$(INST)C")
+dbLoadRecords("db/calculatedWF.db", "AREA=$(AREA), POS=$(POS), INST=$(INST)D")
+
+# Num-sample <--> Time conversion Records supporting ch1 on AMC0, AMC1.
+# Parameterized by per-AMC frequency counter (clock).
+dbLoadRecords("db/blen_gap_weightFunctionXAxis.db", "AREA=$(AREA), POS=$(POS), INST=$(INST)C, AMC=0, CLK_INST=$(INST)A")
+dbLoadRecords("db/blen_gap_weightFunctionXAxis.db", "AREA=$(AREA), POS=$(POS), INST=$(INST)D, AMC=1, CLK_INST=$(INST)B")
+
+# Parse IP address
+dbLoadRecords("db/ipAddr.db", "P=BLEN:$(AREA):$(POS), SRC=ServerRemoteIp")
+
+# Pt100 temperature sensor inputs
+dbLoadRecords("db/blen_gap_temperatures.db", "P=BLEN:${AREA}:${POS}")
 
 # ===========================================
 #               IOC INIT
@@ -52,20 +70,13 @@ cd $(TOP)
 iocInit()
 
 # Enforce RTM timing
-# We're not in slot 2 so use backplane timing
-crossbarControl("FPGA" "BP")
+crossbarControl "FPGA" "$(BLEN_VERSION)"
+
+dbpf TPR:IN10:BL01:5:MODE.PROC 1
 
 # Turn on caPutLogging:
 # Log values only on change to the iocLogServer:
-caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}")
+caPutLogInit("$(EPICS_CA_PUT_LOG_ADDR)")
 caPutLogShow(2)
-
-# blenConfigureMR parameters:
-# 1 - Station name
-# 2 - BSA stream name (must be identical to definition in yaml file)
-# 3 - PV used to get TMIT from FCOM
-# 4 - IP address and port to send TMIT information to ATCA
-# 5 - FCOM Timeout in ms
-blenConfigureMR("BLEN:$(AREA):$(POS)","$(BSA_STREAM_YAML_NAME)","$(TMIT_PV)","$(FPGA_IP):$(IP_PORT_TMIT)", 34)
 
 < iocBoot/common/start_restore_soft.cmd
