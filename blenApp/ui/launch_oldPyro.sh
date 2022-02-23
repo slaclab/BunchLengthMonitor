deff42ff79e8f2ed3bbc5c914ec7706421ca1d04
TaineEriksen <taine@slac.stanford.edu>
2022-02-22 16:44:32 -0800

    New UI

---
+++ blenApp/ui/launch_oldPyro.sh
@@ -0,0 +1,1 @@
+pydm --hide-nav-bar -m "AREA=BC2B,POS=950,INST=BZ21B,IOC_UNIT=BL03,ATCA_SLOT=7,TYPE=BLEN, IOC=sioc-b084-bl03" blen_waveforms.ui
--- blenApp/ui/launch_pyro.sh
+++ blenApp/ui/launch_pyro.sh
@@ -1,1 +1,1 @@
-pydm --hide-nav-bar -m "AREA=BC2B,POS=950,INST=BZ21B,IOC_UNIT=BL03,ATCA_SLOT=7,TYPE=BLEN, IOC=sioc-b084-bl03" blen_waveforms.ui
+pydm --hide-nav-bar -m "AREA=BC2B,POS=950,INST=BZ21B,IOC_UNIT=BL03,ATCA_SLOT=4,TYPE=BLEN, IOC=sioc-b084-bl03, INST_TRIG=4" blen_lcls2_pyro.ui
