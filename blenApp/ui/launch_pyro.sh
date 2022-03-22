b6e5a98fd0e76ae1683022db026476f338824fe2
TaineEriksen <taine@slac.stanford.edu>
2022-03-22 12:07:02 -0700

    New filter control UI

--- blenApp/ui/launch_pyro.sh
+++ blenApp/ui/launch_pyro.sh
@@ -1,1 +1,1 @@
-pydm --hide-nav-bar -m "AREA=BC2B,POS=950,INST=BZ21B,IOC_UNIT=BL03,ATCA_SLOT=4,TYPE=BLEN, IOC=sioc-b084-bl03, INST_TRIG=4" blen_lcls2_pyro.ui
+pydm --hide-nav-bar -m "AREA=BC2B,POS=950,INST=BZ21B,IOC_UNIT=BL03,ATCA_SLOT=4,TYPE=BLEN, IOC=sioc-b084-bl03, INST_TRIG=4, Filtset1=Neutral Density Filters, Filtset2=Low-Pass Filters, Filt1=10 dB, Filt2=Spare Holder, Filt3= Long Bunches, Filt3frq= > 60 fs FWHM ,Filt4=Short Bunches, Filt4frq= 60 fs FWHM" blen_lcls2_pyro.ui
