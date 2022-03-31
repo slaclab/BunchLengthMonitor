dc009dedbe1e19128e151495110de613b2c747bd
TaineEriksen <taine@slac.stanford.edu>
2022-03-31 16:23:24 -0700

    Added calc records to archiver

--- blenApp/scripts/makeArchive.sh
+++ blenApp/scripts/makeArchive.sh
@@ -7,5 +7,5 @@ do
     sleep 1    
 done
 
-cat $IOC_DATA/$1/iocInfo/IOC.pvlist | grep -v "wave" | grep -v string | grep -v subArray | grep -v sub | grep -v calc | grep -v bsa | grep -v bsa | grep -v compress | grep -v fanout | grep -v seq | cut -f1 -d "," | sed 's/$/ 10 scan/' > $IOC_DATA/$1/archive/MAIN.archive
+cat $IOC_DATA/$1/iocInfo/IOC.pvlist | grep -v "wave" | grep -v string | grep -v subArray | grep -v sub | grep -v bsa | grep -v compress | grep -v fanout | grep -v seq | cut -f1 -d "," | sed 's/$/ 10 scan/' > $IOC_DATA/$1/archive/MAIN.archive
 cat $IOC_DATA/$1/iocInfo/IOC.pvlist | grep bsa | grep 1H | cut -f1 -d "," | sed 's/$/ 1 scan/' > $IOC_DATA/$1/archive/LCLS1BSA.archive
